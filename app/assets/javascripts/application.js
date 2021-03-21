48f7d40adfbb1fa4e45e21a0384c7640db0bc1a1
Sa110-5325 <sa110.35325@gmail.com>
2021-03-21 15:39:31 +0000

    seedファイル

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -48,3 +48,14 @@ $(document).ready(function () {
     hidePrevious : false
   });
 });
+
+$(document).ready(function() {
+  $(".menu-trigger").click(function () {
+    $(this).toggleClass("active");
+    $(this).next().toggleClass("onanimation");
+    $('ul li').hide();
+    $('ul li').each(function(i) {
+      $(this).delay(80 * i).fadeIn(500);
+    });
+  });
+});
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ User.create!(
+    name: '齋藤',
+    email: 'sa110.35325@gmail.com',
+    password: 'sa1105325',
+  )
